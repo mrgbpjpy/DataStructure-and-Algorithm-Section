003d678cac40ce00a57451652d0e64c0206eecc8
mrgbpjpy <mrgbpjpy@gmail.com>
2026-01-21 10:33:30 -0500

    added Problem 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Problem1 from './components/Problem_1/Problem1'
 import Problem2 from './components/Problem_2/Problem2'
+import Problem3 from './components/Problem_3/Problem3'
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <h1>70+ JavaScript Challenges - Data Structures and Algorithms</h1>
       <Problem1 />
       <Problem2 />
+      <Problem3 />
     </>
   )
 }
